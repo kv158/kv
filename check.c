dc051e39091bdc74dacc321a2326cc6bc1367035
kv158 <108169728+kv158@users.noreply.github.com>
2022-09-27 15:09:18 +0530

    Add files via upload

---
+++ check.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include<string.h>
+
+
+int main()
+{
+char a[]="++";
+    // String in="+";
+    // scanf("ENTRE INPUT &c",a);
+
+    switch(a[]){
+
+        case "++":
+        printf("this is an aditional operator");
+        break;
+        //  case '/':
+        // printf("this is an division operator");
+        // break;
+        //  case '-':
+        // printf("this is an substraction operator");
+        // break;
+        //  case '*':
+        // printf("this is an multiplication operator");
+        break;
+        //  case '<=':
+        // printf("this is an lessthen equal operator");
+        // break; 
+        //  case '>=':
+        // printf("this is an graterorequal operator");
+        // break;
+
+        //  case '!=':
+        // printf("this is an not equal operator");
+        // break;
+
+    }
+}
+// return 0;
+// }
+// #include <stdio.h>
+// int main()
+// {
+// printf("this is an not equal operator");
+// return 0;
+// }
